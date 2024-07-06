c0a5d0cd048016f915bd0860fdc830a4ca2ea829
lmntrx-sys <infinitiecitie@gmail.com>
2024-07-06 13:18:44 +0300

    update

--- DDPM/Difussion.py
+++ DDPM/Difussion.py
@@ -1,78 +1,40 @@
 import torch
-import numpy as np
+import torch.nn.functional as F
+device = "cuda" if torch.cuda.is_available() else "cpu"
 
-# Define a linear alpha schedule for simplicity
-alpha_schedule = np.linspace(0.001, 1.0, 1000)
+def linear_beta_schedule(timesteps, start=0.0001, end=0.02):
+   return torch.linspace(start, end, timesteps)
 
-def forward_diffusion(img, dim, alpha_schedule):
-    """
-    Reshape the image tensor based on the dim parameter.
-    
-    Parameters:
-    img: torch.Tensor - The input image tensor.
-    dim: tuple - The dimensions to reshape the image tensor.
-    alpha_schedule: list - The list of alpha values for each timestep.
-    
-    Returns:
-    torch.Tensor - The image tensor after applying forward diffusion and the current timestep.
-    """
-    # Reshape the image tensor based on the dim parameter
-    img = img.reshape(-1, *dim)
-    
-    # Normalize the image to be in the range [-1, 1]
-    img = 2 * img - 1
-    
-    # Get the previous time step of the image
-    prev_img = img
-    
-    # Get the time step
-    t = torch.randint(1, len(alpha_schedule), (1,)).item()
-    
-    # Get the noise at timestep t
-    noise = torch.randn_like(img)
-    
-    # Get alpha_t for the current timestep
-    alpha_t = alpha_schedule[t]
-    
-    # Add the noise to the image
-    img = torch.sqrt(alpha_t) * prev_img + torch.sqrt(1 - alpha_t) * noise
+def get_index_from_list(vals, t, x_shape):
+  """
+  Return a specific index t of passed list of values vals
+  while considering the batch dimension
+  """
+  batch_size = t.shape[0]
+  out = vals.gather(-1, t.cpu())
+  return out.reshape(batch_size,  *((1, ) * (len(x_shape) - 1))).to(device)
 
-    return img, t
+def forward_diffusion(x_0, t, device="cpu"):
+  """
+  Performs a single forward diffusion step.
+  """
+  noise = torch.randn_like(x_0)
+  sqrt_alphas_cumprod_t = get_index_from_list(sqrt_alphas_cumprod, t, x_0.shape)
+  sqrt_one_minus_alphas_cumprod_t = get_index_from_list(sqrt_one_minus_alphas_cumprod, 
+                                        t, x_0.shape)
+  # mean + variance
+  return sqrt_alphas_cumprod_t.to(device) * x_0.to(device) \
+   + sqrt_one_minus_alphas_cumprod_t.to(device) * noise.to(device), noise.to(device)
 
-def reverse_diffusion(img, dim, alpha_schedule, beta_schedule):
 
-    """
-    Reshape the image tensor based on the dim parameter.
-    
-    Parameters:
-    img: torch.Tensor - The input image tensor.
-    dim: tuple - The dimensions to reshape the image tensor.
-    alpha_schedule: list - The list of alpha values for each timestep.
-    beta_t: float - The value of beta_t for the current timestep (default is 0.0001).
-    
-    Returns:
-    torch.Tensor - The image tensor after applying reverse diffusion.
-    """
+T = 200
+betas = linear_beta_schedule(timesteps=T)
 
-    # Reshape the image tensor based on the dim parameter
-    img = img.reshape(-1, *dim)
+alphas = 1. - betas
+alphas_cumprod = torch.cumprod(alphas, axis=0)
+alphas_cumprod_prev = F.pad(alphas_cumprod[:-1], (1, 0), value=1.0)
+sqrt_recip_alphas = torch.sqrt(1.0 / alphas)
+sqrt_alphas_cumprod = torch.sqrt(alphas_cumprod)
+sqrt_one_minus_alphas_cumprod = torch.sqrt(1.0 - alphas_cumprod)
+posterior_variance = betas * (1. - alphas_cumprod_prev) / (1. - alphas_cumprod)
 
-    #get the previous time step of the image
-    first_term = torch.sqrt(1 - beta_t) * img
-
-    # Get the time step
-    t = torch.randint(1, len(alpha_schedule), (1,)).item()
-
-    # Get the noise at timestep t
-    noise = torch.randn_like(img)
-
-    # Get alpha_t for the current timestep
-    alpha_t = alpha_schedule[t]
-
-    # Get beta_t for the current timestep
-    beta_t = beta_schedule[t]
-
-    # Add the noise to the image
-    img = first_term + torch.sqrt(beta_t / alpha_t) * noise
-
-    return img
--- DDPM/model.py
+++ DDPM/model.py
@@ -14,113 +14,35 @@ import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
 
-class Encoder(nn.Module):
+class Block(nn.Module):
 
-    def __init__(self, inputs, num_filters):
-        """
-        Constructor for the Encoder class.
+    def __init__(self, in_ch, out_ch, time_embed_dim, up=False):
+        super().__init__()
+        self.time_mlp = nn.Linear(time_embed_dim, out_ch)
+        if up:
+            self.conv1 = nn.Conv2d(2*in_ch, out_ch, 3, padding=1)
+            self.transform = nn.ConvTranspose2d(in_ch, out_ch, 4, 2, 1)
+        else:
+            self.conv1 = nn.Conv2d(in_ch, out_ch, 3, padding=1)
+            self.transform = nn.Conv2d(in_ch, out_ch, 4, 2, 1)
+        self.conv2 = nn.Conv2d(out_ch, out_ch, 3, padding=1)
+        self.bnorm1 = nn.BatchNorm2d(out_ch)
+        self.pool = nn.MaxPool2d(3, stride=2)
+        self.relu = nn.ReLU()
 
-        Args:
-            inputs (int): The number of input channels for the encoder.
-            num_filters (int): The number of filters for the convolutional layers.
-
-        Returns:
-            None
-        """
-        super(Encoder, self).__init__()
-
-        # Convolution with 3x3 filter followed by ReLU activation
-        self.conv1 = nn.Conv2d(inputs, num_filters, kernel_size=3, padding=1)
-        self.relu1 = nn.ReLU()
-        self.conv2 = nn.Conv2d(num_filters, num_filters, kernel_size=3, padding=1)
-        self.relu2 = nn.ReLU()
-
-        # Max pooling
-        self.maxpool = nn.MaxPool2d(kernel_size=2, stride=2)
-
-    def forward(self, x):
-        x = self.conv1(x)
-        x = self.relu1(x)
-
-        x = self.conv2(x)
-        x = self.relu2(x)
+    def forward(self, x, t, ):
+        # First conv
+        h = self.bnorm1(self.relu(self.conv1(x)))
+        # First, we compute the time embedding 
+        t = self.relu(self.time_mlp(t))
+        # Add time channel
+        t = t.unsqueeze(2).unsqueeze(3)
+        h = h + t
+        # Second conv
+        h = self.relu(self.bnorm1(self.conv2(h)))
+        # Downsample
+        h = self.pool(h)
+        # Transform
+        h = self.transform(h)
+        return h
         
-        x = self.maxpool(x)
-
-        return x
-    
-class Decoder(nn.Module):
-
-    def __init__(self, inputs, num_filters):
-        """
-        Initializes a Decoder object with the given inputs and num_filters.
-
-        Args:
-            inputs (int): The number of input channels for the decoder.
-            num_filters (int): The number of filters for the convolutional layers.
-
-        Returns:
-            None
-        """
-        super(Decoder, self).__init__()
-
-        # Upsampling
-        self.upsample = nn.Upsample(scale_factor=2, mode='nearest')
-
-        # Convolution with 3x3 filter followed by ReLU activation
-        self.conv1 = nn.Conv2d(inputs, num_filters, kernel_size=3, padding=1)
-        self.relu1 = nn.ReLU()
-        self.conv2 = nn.Conv2d(num_filters, num_filters, kernel_size=3, padding=1)
-        self.relu2 = nn.ReLU()
-
-    def forward(self, x):
-        x = self.upsample(x)
-
-        x = self.conv1(x)
-        x = self.relu1(x)
-
-        x = self.conv2(x)
-        x = self.relu2(x)
-
-        return x
-    
-class UNet(nn.Module):
-
-    def __init__(self, num_filters):
-        """
-        Initializes a UNet model with the specified number of filters.
-
-        Args:
-            num_filters (int): The number of filters to use in the model.
-
-        """
-        super(UNet, self).__init__()
-
-        self.encoder1 = Encoder(3, num_filters)
-        self.encoder2 = Encoder(num_filters, num_filters * 2)
-        self.encoder3 = Encoder(num_filters * 2, num_filters * 4)
-        self.encoder4 = Encoder(num_filters * 4, num_filters * 8)
-
-        self.decoder1 = Decoder(num_filters * 8, num_filters * 4)
-        self.decoder2 = Decoder(num_filters * 4, num_filters * 2)
-        self.decoder3 = Decoder(num_filters * 2, num_filters)
-        self.decoder4 = Decoder(num_filters, num_filters)
-
-        self.final_conv = nn.Conv2d(num_filters, 3, kernel_size=1)
-
-    def forward(self, x):
-        # Encoder
-        enc1 = self.encoder1(x)
-        enc2 = self.encoder2(enc1)
-        enc3 = self.encoder3(enc2)
-        enc4 = self.encoder4(enc3)
-
-        # Decoder
-        dec1 = self.decoder1(enc4)
-        dec2 = self.decoder2(dec1)
-        dec3 = self.decoder3(dec2)
-        dec4 = self.decoder4(dec3)
-
-        out = self.final_conv(dec4)
-
-        return out
